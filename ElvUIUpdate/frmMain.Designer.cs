d44594f52e6982e6be321b7d2aca7cdf6da9aa3d
Paulo H V Neves <paulodemoc@gmail.com>
2017-12-15 10:24:27 -0200

    ElvUI Update Initial Commit

---
+++ ElvUIUpdate/frmMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ElvUIUpdate
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pgBar = new System.Windows.Forms.ProgressBar();
+            this.lbProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pgBar
+            // 
+            this.pgBar.Location = new System.Drawing.Point(12, 29);
+            this.pgBar.Name = "pgBar";
+            this.pgBar.Size = new System.Drawing.Size(440, 23);
+            this.pgBar.Step = 1;
+            this.pgBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pgBar.TabIndex = 0;
+            // 
+            // lbProgress
+            // 
+            this.lbProgress.AutoSize = true;
+            this.lbProgress.Location = new System.Drawing.Point(17, 9);
+            this.lbProgress.Name = "lbProgress";
+            this.lbProgress.Size = new System.Drawing.Size(0, 13);
+            this.lbProgress.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(464, 63);
+            this.Controls.Add(this.lbProgress);
+            this.Controls.Add(this.pgBar);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "ElvUI Updater";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgBar;
+        private System.Windows.Forms.Label lbProgress;
+    }
+}
+
---
+++ ElvUIUpdate/frmMain.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace ElvUIUpdate
+{
+    public partial class frmMain : Form
+    {
+        const string CONFIGKEYPATH = "wowpath";
+        const string TUKUIURL = "https://www.tukui.org/welcome.php";
+        string wowPath;
+        string addonsFolder;
+        string localFilePath;
+        System.Collections.Specialized.NameValueCollection appSettings;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            loadSettings();
+            downloadZip();
+        }
+
+        private void downloadZip()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(TUKUIURL);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                if (!string.IsNullOrEmpty(data))
+                {
+                    int startPos = -1, endPos = -1;
+                    startPos = data.IndexOf("<a href=\"/downloads/elvui");
+                    if (startPos != -1)
+                    {
+                        endPos = data.IndexOf('"', startPos + 10);
+                        if (endPos != -1)
+                        {
+                            string zipUrl = @"https://www.tukui.org/" + data.Substring(startPos + 10, endPos - startPos - 10);
+                            string zipFileName = zipUrl.Split('/').Last();
+                            localFilePath = System.IO.Path.GetTempPath() + zipFileName;
+                            using (WebClient wc = new WebClient())
+                            {
+                                wc.DownloadProgressChanged += (s, e) =>
+                                {
+                                    pgBar.Value = e.ProgressPercentage;
+                                };
+                                wc.DownloadFileCompleted += (s, e) =>
+                                {
+                                    DownloadFileCompleted();
+                                };
+                                lbProgress.Text = "Downloading ElvUI";
+                                wc.DownloadFileAsync(new Uri(zipUrl), localFilePath);
+                            }
+                        }
+                    }
+                }
+
+                response.Close();
+                readStream.Close();
+            }
+
+        }
+
+        private async void DownloadFileCompleted()
+        {
+            pgBar.Value = 0;
+            lbProgress.Text = "Extracting Files";
+            if (!string.IsNullOrEmpty(localFilePath))
+            {
+                using (ZipInputStream zipStream = new ZipInputStream(File.OpenRead(localFilePath)))
+                {
+                    ZipEntry zipEntry;
+                    while ((zipEntry = zipStream.GetNextEntry()) != null)
+                    {
+                        pgBar.Value = Decimal.ToInt32(204800 / (new System.IO.FileInfo(localFilePath).Length));
+                        string directoryName = Path.GetDirectoryName(zipEntry.Name);
+                        string fileName = Path.GetFileName(zipEntry.Name);
+
+                        if (directoryName.Length > 0 && !Directory.Exists(addonsFolder + "\\" + directoryName))
+                        {
+                            Directory.CreateDirectory(addonsFolder + "\\" + directoryName);
+                        }
+
+                        if (fileName != String.Empty)
+                        {
+                            using (FileStream streamWriter = File.Create(addonsFolder + "\\" + zipEntry.Name.Replace("/", @"\")))
+                            {
+                                int size = 2048;
+                                byte[] data = new byte[2048];
+                                while (true)
+                                {
+                                    size = zipStream.Read(data, 0, data.Length);
+                                    if (size > 0)
+                                    {
+                                        streamWriter.Write(data, 0, size);
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            pgBar.Value = 100;
+            lbProgress.Text = "Finished";
+            await Task.Delay(5000);
+            this.Close();
+        }
+
+        private void loadSettings()
+        {
+            appSettings = ConfigurationManager.AppSettings;
+            wowPath = appSettings[CONFIGKEYPATH] ?? string.Empty;
+            if (string.IsNullOrEmpty(wowPath))
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    fbd.Description = "Please inform your WoW installation folder:";
+                    DialogResult fbdReturn = fbd.ShowDialog();
+
+                    if (fbdReturn == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        wowPath = fbd.SelectedPath;
+                        Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                        config.AppSettings.Settings.Add(CONFIGKEYPATH, wowPath);
+                        config.Save(ConfigurationSaveMode.Minimal);
+                    }
+                }
+            }
+
+            addonsFolder = wowPath + "\\Interface\\Addons";
+        }
+    }
+}
